413b19893dd68fdb7547f0c80c15fc0dd89d8a03
Melonkon <qridderpl@gmail.com>
2023-01-19 23:27:04 +0100

    Laatste aanpassingen + docstrings

--- download.sh
+++ download.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+rm GCF*
 python3 sql.py 1
 
 var="$(cat temp.txt)"\
@@ -10,20 +10,20 @@ then
         gunzip *.gz
         mv *.faa OrnAna.fa
         makeblastdb -in OrnAna.fa -dbtype prot -parse_seqids
-        blastx -query T1.fa -db OrnAna.fa -outfmt 6 -out total.txt
-        sort -k1,1 -k11,11g total.txt | sort --merge -u  -k1,1 > totaal_sort.txt
-        cat total_sort.txt | awk '{print $2}' > ensemble_ids.txt
+        blastx -query T1.fa -db OrnAna.fa -outfmt 6 -out totaal.txt
+        sort -k1,1 -k11,11g -k3,3nr totaal.txt | sort --merge -u -k1,1 > totaal_sort.txt
+
+        cat totaal_sort.txt | awk '{print $2}' > ensemble_ids.txt
         grep -oE "^>\w+" OrnAna.fa | sed 's/>//g' >> proteoom_ids.txt
+        rm Orn*
+        rm temp.txt
+        rm esum*
+        rm tot*
 
         python3 sql.py 2;
 
         rm proteoom_ids.txt
 	rm output.txt
-        rm Orn*
-        rm tot*
-        rm temp.txt
-        rm esummary*
         rm ensemble_ids.txt
-	rm esum*
 fi
 
